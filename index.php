76a5130c5acd465682203dc0ad2cd69c8b5f02a5
mujiantomainong <46754358+mujiantomainong@users.noreply.github.com>
2019-01-16 23:53:09 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<table width="337">
+
+<td width="298" height="300" align="center"> <p><strong>Nama : Mujianto mainong </strong></p>
+<p><strong> Nim : 215280041 </strong></p>
+<p><strong> Phone : 082292733044 </strong></p>
+<p><strong> Fax : mujiantomainong3@gmail.com </strong></p>
+<p><strong> Sulawesi Selatan </strong></p>
+</table>
